dfc74ab6d649b555575655322823d43ebd8f9454
Byran Wills-Heath <byran@bluefruit.co.uk>
2015-09-30 17:10:43 +0100

    End of first training session

--- TestDoublePracticeTests/HelloTests.cpp
+++ TestDoublePracticeTests/HelloTests.cpp
@@ -2,7 +2,44 @@
 
 using namespace ::testing;
 
-TEST(Hello, World)
+class I2CInterface
 {
+public:
+	virtual ~I2CInterface(){}
+	virtual void Start() = 0;
+	virtual bool Write(char data) = 0;
+	virtual void Stop() = 0;
+};
 
+class BMA150Accelerometer
+{
+public:
+	explicit BMA150Accelerometer(I2CInterface*){  }
+	void SelectXRegister(){}
+};
+
+class I2CDouble : public I2CInterface
+{
+public:
+	MOCK_METHOD0(Start,void ());
+	MOCK_METHOD1(Write, bool (char data));
+	MOCK_METHOD0(Stop, void ());
+};
+
+TEST(BMA150Accelerometer, Selecting_the_X_register_sends_the_correct_I2C_commands)
+{
+	//Given
+	I2CDouble i2CDouble;
+	BMA150Accelerometer accelerometer(&i2CDouble);
+
+	EXPECT_CALL(i2CDouble, Start());
+	EXPECT_CALL(i2CDouble, Write(0x38 | 0x01)).WillOnce(Return(true));
+	EXPECT_CALL(i2CDouble, Write(0x02)).WillOnce(Return(true));
+	EXPECT_CALL(i2CDouble, Stop());
+
+	//When
+	accelerometer.SelectXRegister();
+
+	//Then
+	
 }
